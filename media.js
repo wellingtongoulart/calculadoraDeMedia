f74c160e85d75a732a6385c08cfbd04c57d3cd7a
wellingtongoulart <wellingtongoulart@outlook.com>
2021-09-16 19:33:33 -0300

    jsfinal

---
+++ media.js
@@ -0,0 +1,11 @@
+var notaDoPrimeiroBimestre= 8.3
+var notaDoSegundoBimestre= 6.4
+var notaDoTerceiroBimestre= 8.8
+var notaDoQuartoBimestre= 7.9
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+  
+ var notaFixada = notaFinal.toFixed(1)
+  
+  
+console.log(notaFinal)
